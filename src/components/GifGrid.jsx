262f1949170c91acc041ce7e161aa3ddd64ee9eb
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-09 13:42:29 -0300

    -- Fetch --

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,16 @@
 const GifGrid = ({ category }) => {
-  return <>
-    <h3>{category}</h3>
-    <p>Hola Mundo</p>
-  </>;
+  const getGif = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=gc71ip42kT92TPgI6f04gy9G9xTWTj6c&q=${category}`;
+    const respuesta = await fetch(url)
+    const {data} = await respuesta.json()
+  };
+  getGif()
+  return (
+    <>
+      <h3>{category}</h3>
+      <p>Hola Mundo</p>
+    </>
+  );
 };
 
 export default GifGrid;
